f374f4c7278ab90e357a3fc95ffb2c2ac77f8160
Harrison Fung <harrison.hok.fung@gmail.com>
2021-01-21 14:56:13 -0500

    changes

--- logic.js
+++ logic.js
@@ -27,6 +27,28 @@ function decideWinner( computer, user ){
     return winner
 }
 
+function start(){
+
+    var choices = ["rock", "paper", "scissors"];
+    var choice = Math.floor(Math.random()*choices.length);
+    var compChoice = choices[choice];
+    var computerChoice = document.getElementById("computerChoice");
+    var alertArea = document.getElementById("winner");
+    var rockAnswer = document.getElementById("rock");
+    var paperAnswer = document.getElementById("paper");
+    var scissorsAnswer = document.getElementById("scissors");
+
+    rockAnswer.addEventListener("onkeypress", function(){
+        if (compChoice === "paper"){
+            alertArea.textContent = "Winner";
+            computerChoice.textContent = "Paper";
+        }
+    })
+}
+
+start();
+
+
 // some TESTING
 // var computer = 'rock'
 // var user = 'rock'
