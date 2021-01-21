91a0477f1f31a13de9fb1ffc0f3e0ec67de46be5
Luca <lucahbo@hotmail.com>
2021-01-21 14:41:57 -0500

    Created variables and created functions

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// Take values from buttons
+//show win tie and losses counters
+// variable my user choice
+// variable the computer choice
+// and some prompt to play again
+
+var wins = 0;
+var lose = 0;
+var tie = 0;
+var computerChoices = ["rock", "paper", "scissors"];
+var userChoices;
+
+function getRock() {
+  //give value to userChoices with rock button
+}
+
+function getPaper() {
+  //give value to userChoices with paper button
+}
+
+function getScissors() {
+  //give value to userChoices with scissors button
+}
+
+function playGame() {}
