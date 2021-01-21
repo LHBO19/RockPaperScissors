ca00ac12d4e06b4757dfaad2408d1629144512f4
Luca <lucahbo@hotmail.com>
2021-01-21 15:09:37 -0500

    all files together

--- javascript.js
+++ javascript.js
@@ -1,21 +1,20 @@
-console.log("In console")
-function start(){
+console.log("In console");
+function start() {
+  var choices = ["rock", "paper", "scissors"];
+  var choice = Math.floor(Math.random() * choices.length);
+  var compChoice = choices[choice];
+  var computerChoice = document.getElementById("computerChoice");
+  var alertArea = document.getElementById("winner");
+  var rockAnswer = document.getElementById("rock");
+  var paperAnswer = document.getElementById("paper");
+  var scissorsAnswer = document.getElementById("scissors");
 
-    var choices = ["rock", "paper", "scissors"];
-    var choice = Math.floor(Math.random()*choices.length);
-    var compChoice = choices[choice];
-    var computerChoice = document.getElementById("computerChoice");
-    var alertArea = document.getElementById("winner");
-    var rockAnswer = document.getElementById("rock");
-    var paperAnswer = document.getElementById("paper");
-    var scissorsAnswer = document.getElementById("scissors");
-
-    rockAnswer.addEventListener("onkeypress", function(){
-        if (compChoice === "paper"){
-            alertArea.textContent = "Winner";
-            computerChoice.textContent = "Paper";
-        }
-    })
+  rockAnswer.addEventListener("click", function () {
+    if (compChoice === "paper") {
+      alertArea.textContent = "Winner";
+      computerChoice.textContent = "Paper";
+    }
+  });
 }
 
-start()
+start();
